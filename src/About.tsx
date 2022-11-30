ff57edd1fca87b8f441aa6dff7c58258b043e766
Michael Timbrook <timbrook480@gmail.com>
2022-11-30 11:22:01 -0800

    Blurbs

--- src/About.tsx
+++ src/About.tsx
@@ -10,12 +10,18 @@ function About() {
     <>
       <article className="about">
         I've been an engineer at Instagram for {yearsAtIG} years doing release
-        and developer experience.
+        and developer experience. As well as working on core messaging stack and communities.
       </article>
       <article className="about">
         On my own I run a home network with 5 rpis + proxmox vm running
         everything from qemu to nomad and k8s. It's where I play around and
         learn the latest in FOSS infra software.
+      </article>
+      <article className="about">
+        "I think Michael has the unique mix of enthusiasm, technical skills, and context 
+        to be able to take infrastructure and projects from our team and surface them to developers 
+        in a delightful way, and the ability to bring back the needs of developers and use that to 
+        influence our priorities." ~My Team
       </article>
       <article className="about">
         If it's not being continually built and released is it even real
