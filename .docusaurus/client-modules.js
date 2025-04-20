7f383c84ed7ab0a91f080693b92076d01ee1e445
7imbrook <timbrook480@gmail.com>
2025-04-20 13:36:16 -0700

    Move new generator up to root of directory removing old system

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/michael/timbrook.tech/node_modules/infima/dist/css/default/default.css"),
+  require("/home/michael/timbrook.tech/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/michael/timbrook.tech/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/michael/timbrook.tech/src/css/custom.css"),
+];
---
+++ .docusaurus/routes.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
+    exact: true
+  },
+  {
+    path: '/about',
+    component: ComponentCreator('/about', '954'),
+    exact: true
+  },
+  {
+    path: '/blog',
+    component: ComponentCreator('/blog', 'b6e'),
+    exact: true
+  },
+  {
+    path: '/blog/2025/adding-a-blog',
+    component: ComponentCreator('/blog/2025/adding-a-blog', '60a'),
+    exact: true
+  },
+  {
+    path: '/blog/archive',
+    component: ComponentCreator('/blog/archive', '182'),
+    exact: true
+  },
+  {
+    path: '/blog/authors',
+    component: ComponentCreator('/blog/authors', '0b7'),
+    exact: true
+  },
+  {
+    path: '/',
+    component: ComponentCreator('/', 'e5f'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*'),
+  },
+];
